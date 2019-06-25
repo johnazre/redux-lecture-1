8369ca356123465f1aeab0026f4edc56724f2bd8
johnazre <jaboyak@gmail.com>
2019-06-25 10:47:02 -0700

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,10 @@ import TodosList from './components/TodosList'
 import { ITodo } from './types'
 
 const App: React.FC = () => {
-  let someTodos: ITodo[] = []
-  const [todos, setTodos] = useState(someTodos)
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/todos`)
-      .then(res => res.json())
-      .then(todos => setTodos(todos))
-  }, [])
-
-  function addTodo(title: string) {
-    fetch(`https://jsonplaceholder.typicode.com/todos`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ title })
-    })
-      .then(res => res.json())
-      .then((newTodo: ITodo) => {
-        let updatedTodos: ITodo[] = [...todos, newTodo]
-        setTodos(updatedTodos)
-      })
-  }
-
   return (
     <div className="App">
       <TopNav />
-      <TodosList todos={todos} />
+      <TodosList />
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,12 @@ import './index.css'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
